b1cd2745c05360d092d9055ecd17f3b24f4acc13
Lepcha <nissan.lepcha@stu.jefferson.kyschools.us>
2018-09-24 08:19:37 -0400

    first

---
+++ dictionary.py
@@ -0,0 +1,45 @@
+math_game = {
+        'title':'title',
+        'type':'type',
+        'played by':'username',
+        'level':'blank',
+        'game_state':True,
+        } 
+
+
+while True:
+        math_game['player'] = input("what is your username?")
+        
+        print(math_game)
+
+        for key,value in math_game.items():
+            print(f"{key}: {value}")
+
+# chess game
+import random
+
+chess_moves = []
+for value in (0,100):
+        chess_turns.append(random.randint(1,10))
+
+        print(chess_moves)
+        
+        Player1 = {
+        'name': "nishan",
+        'age': 55,
+}
+
+
+while True:
+        #a variable of user_inputs that stores the value from input ()
+        new_name = input("what is your name? ")
+        player1['name'] = new_name
+
+        roll_command = input("Press shift to make a chess move!: ")
+        chess_moves = random.randit(1.100)
+        print("You moved a %s " % chess_move)
+
+            
+            
+
+
